540399ad3b652194a39bf3cf4c0a4887ccd5a767
v08nike <teddan817@gmail.com>
2024-10-16 09:50:09 +0800

    chore: adjusted style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
+import { debounce } from "lodash";
 import dynamic from "next/dynamic";
+import { useEffect } from "react";
 
 const CMS = dynamic(() => import("decap-cms").then((mod: any) => mod.CMS), {
   ssr: false,
@@ -6,6 +8,49 @@ const CMS = dynamic(() => import("decap-cms").then((mod: any) => mod.CMS), {
 });
 
 export default function Home() {
+  useEffect(() => {
+    const applyIframeStyles = (iframe: HTMLIFrameElement) => {
+      const iframeDoc =
+        iframe.contentDocument || iframe.contentWindow?.document;
+      if (iframeDoc) {
+        // Apply styles dynamically to the iframe content
+        const style = document.createElement("style");
+        style.textContent = `
+          img {
+            max-width: 100%;
+            height: auto;
+          }
+        `;
+        iframeDoc.head.appendChild(style);
+      }
+    };
+
+    const checkAndApplyStyles = debounce(() => {
+      const iframe = document.getElementById(
+        "preview-pane"
+      ) as HTMLIFrameElement;
+      if (iframe) {
+        applyIframeStyles(iframe);
+      }
+    }, 200);
+
+    // Set up a MutationObserver to watch for changes in the DOM
+    const observer = new MutationObserver(() => {
+      checkAndApplyStyles();
+    });
+
+    // Start observing the body for changes
+    const observerConfig = { childList: true, subtree: true };
+    const targetNode = document.body; // Observe changes in the entire document
+    observer.observe(targetNode, observerConfig);
+
+    // Initial check in case the iframe already exists when the component loads
+    checkAndApplyStyles();
+
+    // Cleanup observer on component unmount
+    return () => observer.disconnect();
+  }, []);
+
   return (
     <div className="flex items-center justify-center">
       <CMS />
